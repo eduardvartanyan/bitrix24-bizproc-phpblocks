ef6e315567290c71038493c79f3412173931d1fe
evartanyan1989 <evartanyan1989@gmail.com>
2025-09-14 16:30:58 +0800

    Position — Добавлен в БП 612 блок Формируем список заказов клиентов

---
+++ local/php_interface/lib/biz-proc-php-blocks/positions/612-generate-deal-list.php
@@ -0,0 +1,29 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/bitrix/modules/main/include/prolog_before.php');
+
+// Формируем список заказов клиентов
+
+use Bitrix\Crm\DealTable;
+use Bitrix\Main\Loader;
+use Bitrix\Main\LoaderException;
+
+try {
+    Loader::includeModule('crm');
+} catch (LoaderException $e) { return; }
+
+$obElementList = CIBlockElement::GetList(
+    [],
+    ['IBLOCK_ID' => 20, 'ID' => '{{ID элемента}}'],
+    false,
+    false,
+    ['PROPERTY_ZAKAZ_KLIENTA']
+);
+
+$html = '';
+while ($arElement = $obElementList->fetch()) {
+    $arDeal = DealTable::getById($arElement['PROPERTY_ZAKAZ_KLIENTA_VALUE'])->fetch();
+    $html .= '<a href="https://ipvartanyan.ru/crm/deal/details/' . $arElement['PROPERTY_ZAKAZ_KLIENTA_VALUE'] . '/">'
+        . $arDeal['TITLE'] . '</a> (ID ' . $arElement['PROPERTY_ZAKAZ_KLIENTA_VALUE'] . ')<br><br>';
+}
+
+$this->SetVariable('html', $html);
